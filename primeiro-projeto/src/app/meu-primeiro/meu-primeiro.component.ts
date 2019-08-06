38e8f747d2123c30ec5687d4a25ff9b240762d81
gaahbriel9595 <43424762+gaahbriel9595@users.noreply.github.com>
2019-08-06 03:13:21 -0300

    Add files via upload

---
+++ primeiro-projeto/src/app/meu-primeiro/meu-primeiro.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+	selector: 'meu-primeiro-component',
+	template: `
+	<h1> Meu primeiro component com Angular 2! </h1>
+	`
+})
+
+export class MeuPrimeiroComponent{
+	
+}
